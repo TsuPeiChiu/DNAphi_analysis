886a330e845c23ee43b78e231f9f6ac120c28f5e
TsuPeiChiu <tsupeich@usc.edu>
2017-10-25 15:22:45 -0700

    updated code

--- run_MLR.r
+++ run_MLR.r
@@ -1,32 +1,27 @@
 ##
 # DNAphi analysis
 ##
-library(DNAphiR)
+library(DNAshapeR)
 library(caret)
 library(ggplot2)
 library(grid)
 
 ## Set parameters
-groupName <- "group"
-folderPath <- paste0( "file_path", groupName )
+groupName <- "test"
+folderPath <- "C:\\Users\\tsupeich\\Documents\\GitHub\\DNAphi_analysis\\example_datasets\\"
 faFileNames <- list.files(folderPath, pattern = "txt.fa$")
-flagPermute <- 0
+outputFileName <- paste0(format(Sys.time(), format = "%Y-%m-%d-%H%M%S"), ".txt")
 
 featureNames <- list(
-    c("1-MGW"),
-    c("1-EP"),
-    c("1-mer"),
-    c("1-mer", "1-MGW"),
-    c("1-mer", "1-EP"),
-    c("1-mer", "1-shape"),
-    c("1-mer", "1-ProT", "1-Roll", "1-HelT", "1-EP"),
-    c("1-mer", "1-shape", "1-EP"),
-    c("1-mer", "1-EP", "1-MGW")
+  c("1-MGW"),
+  c("1-EP"),
+  c("1-mer"),
+  c("1-mer", "1-MGW"),
+  c("1-mer", "1-EP"),
+  c("1-mer", "1-shape"),
+  c("1-mer", "1-ProT", "1-Roll", "1-HelT", "1-EP"),
+  c("1-mer", "1-shape", "1-EP")
 )
-
-outputFileName <- paste0(format(Sys.time(), format = "%Y-%m-%d-%H%M%S"), ".txt")
-weightFileName <- paste0(folderPath, "\\", outputFileName, ".weighted.txt")
-
 
 ## Run prediction
 i <- 0
@@ -37,46 +32,41 @@ for(faFileName in faFileNames){
     fn <- paste0(folderPath,"\\", faFileName)
     print(featureName)
     print(faFileName)
-
+    
     print("shape prediction...")
     pred <- getShape(fn)
-
+    
     print("encoding...")
     featureVector <- encodeSeqShape(fn, pred, featureName, normalize = TRUE)
-    write.table(featureVector, file = paste0(folderPath,"\\", outputFileName, ".", faFileName, ".", paste(featureName, collapse="_"), ".feature.txt"))
-
+    
     print("machine learning...")
     experimentalData <- read.table( sub("\\.fa", replacement = "", fn) )
     df <- data.frame(affinity=experimentalData$V2, featureVector)
-
+    
     set.seed(168)
     trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
     model <- train(affinity~ ., data = df, trControl=trainControl, method="glmnet",
                    tuneGrid=data.frame(alpha = 0, lambda=c(2^c(-15:15))))
-
+    
     rs <- model$results$Rsquared[1]
     rsd <- model$results$RsquaredSD[1]
-
+    
     count <- nrow(featureVector)
     result <- paste(faFileName, groupName, count, paste(featureName, collapse="|"), rs, rsd)
-
+    
     write(result, file = paste0(folderPath,"\\", outputFileName), append = TRUE)
-
-    # Write weight file
-    weights <- coef(model$finalModel, model$bestTune$lambda)
-    weights <- as.matrix(weights)
-    write(faFileName, file = weightFileName, append = TRUE)
-    write.table(weights, file = weightFileName, append = TRUE)
+    
   }
 }
 
+# Write output into file
+# Output format:
+# fileName | groupName | count | MGW | EP | seq | seq+MGW | seq+EP | seq+shape | seq+3shape+ep | seq+shape+ep
+fileName <- paste0(folderPath, "\\", outputFileName)
+qt <- read.table(fileName)
 
-## Format output file
-# fileName | groupName | count | MGW | EP | seq | seq+MGW | seq+EP | seq+shape | seq+3shape+ep | seq+shape+ep | 1-mer+1-EP+1-MGW
+for( i in 1:floor(nrow(qt)/8) ){
+  result <- unname( cbind(qt[8*i-7, 1:3], t(qt[(8*i-7):(8*i), 5])) )
+  write.table(result, file = paste0(fileName, ".formated.txt"), append = TRUE, quote = FALSE)
+}
 
-formatedFileName <- paste0(folderPath, "\\", outputFileName, ".formated.txt")
-qt <- read.table( paste0(folderPath,"\\", outputFileName) )
-
-sizeFeature <- length(featureNames)
-
-#####
